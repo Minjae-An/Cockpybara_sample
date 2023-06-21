f78eefa7eb14bfc71a783304ded5f93a97b5321d
calmmin <mj3242@naver.com>
2023-06-22 03:06:12 +0900

    feature : 프로젝트 환경설정 - Java 11 - SpringBoot 2.7.12 - Spring Web - Spring data JPA - Lombok - Spring Security

---
+++ Cockpybara/src/test/java/Alchole_free/Cockpybara/CockpybaraApplicationTests.java
@@ -0,0 +1,13 @@
+package Alchole_free.Cockpybara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockpybaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
